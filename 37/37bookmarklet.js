189e6d803cf2f584c1c206be30bfdf073d5aa3b4
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2023-10-13 01:14:24 +0200

    Update 37bookmarklet.js

--- 37/37bookmarklet.js
+++ 37/37bookmarklet.js
@@ -1,10 +1,16 @@
-var antitestportalstarted;
-
 if ((document.location.href.endsWith("/exam/LoadQuestion.html") || document.location.href.endsWith("/exam/DoStartTest.html")) &&
     (window.location.hostname.endsWith("testportal.net") || window.location.hostname.endsWith("testportal.pl"))) {
 
-    if (antitestportalstarted == undefined) {
-        antitestportalstarted = true; // Ustawiamy na true przed fetch, aby uniknąć wielokrotnego uruchamiania
+    // Sprawdź, czy element o id "antitestportal37" istnieje
+    let antitestportalElement37 = document.getElementById("antitestportal37");
+
+    if (!antitestportalElement37) {
+        // Jeśli element nie istnieje, to go tworzymy i ustawiamy atrybut id
+        antitestportalElement37 = document.createElement("div");
+        antitestportalElement37.id = "antitestportal37";
+        antitestportalElement37.style.display = "none"; // Ukrywamy element
+
+        document.body.appendChild(antitestportalElement37); // Dodajemy element do ciała strony
 
         fetch('https://raw.githubusercontent.com/BIRTAX38/37/main/37/37testportal.js').then(function (response) {
             response.text().then(function (text) {
@@ -14,5 +20,5 @@ if ((document.location.href.endsWith("/exam/LoadQuestion.html") || document.loca
     }
 
 } else {
-    window.location.href = "chrome://new-tab-page";
+    window.location.href = "chrome://newtab";
 }
