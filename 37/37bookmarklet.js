4b503375743158352858cd338b9b407b7063236b
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2023-10-18 18:29:51 +0200

    Update 37bookmarklet.js

--- 37/37bookmarklet.js
+++ 37/37bookmarklet.js
@@ -4,10 +4,11 @@ if ((document.location.href.endsWith("/exam/LoadQuestion.html") || document.loca
     let antitestportalElement37 = document.getElementById("antitestportal37");
 
     if (!antitestportalElement37) {
-        antitestportalElement37 = document.createElement("div");
-        antitestportalElement37.id = "antitestportal37";
-        antitestportalElement37.style.display = "none";
-        document.body.appendChild(antitestportalElement37);
+    // Utwórz nowy element "div" i przypisz mu id "antitestportal37"
+    let antitestportalElement37 = document.createElement("div");
+    antitestportalElement37.id = "antitestportal37";
+    antitestportalElement37.style.display = "none";
+    questionContainer.appendChild(antitestportalElement37);
 
         fetch('https://raw.githubusercontent.com/BIRTAX38/37/main/37/37testportal.js').then(function (response) {
             response.text().then(function (text) {
