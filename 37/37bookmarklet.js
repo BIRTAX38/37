3e1b375a3bdef8dfee86fbdd4a60d42e5d066d5c
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2023-10-19 16:47:51 +0200

    Update 37bookmarklet.js

--- 37/37bookmarklet.js
+++ 37/37bookmarklet.js
@@ -1,5 +1,4 @@
-if ((document.location.href.endsWith("/exam/LoadQuestion.html") || document.location.href.endsWith("/exam/DoStartTest.html") || document.location.href.endsWith("/exam/DoTestQuestion.html")) &&
-    (window.location.hostname.endsWith("testportal.net") || window.location.hostname.endsWith("testportal.pl"))) {
+if ((window.location.hostname.endsWith("testportal.net") || window.location.hostname.endsWith("testportal.pl"))) {
 
     let antitestportalElement37 = document.getElementById("antitestportal37");
 
