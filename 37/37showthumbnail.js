3b37c95d4a84b91a46f6d68afe4a12f1a9c6eda2
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2023-11-04 16:49:31 +0100

    Update 37showthumbnail.js

--- 37/37showthumbnail.js
+++ 37/37showthumbnail.js
@@ -1,14 +1,6 @@
-            // Sprawdzanie aktualnego adresu URL
             var currentURL = window.location.href;
-
-            // Sprawdzanie, czy aktualny adres URL rozpoczyna się od "https://www.youtube.com/watch?"
             if (currentURL.startsWith("https://www.youtube.com/watch?")) {
-                // Pobieranie wartości parametru "v" z adresu URL
                 var videoId = new URL(currentURL).searchParams.get("v");
-
-                // Tworzenie adresu URL docelowego
                 var targetURL = "https://i.ytimg.com/vi/" + videoId + "/maxresdefault.jpg";
-
-                // Przenoszenie na żądaną stronę
                 window.open(targetURL);
-            }
+}
