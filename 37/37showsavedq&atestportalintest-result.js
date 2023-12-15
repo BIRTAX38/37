579475fafdf1ee7d3d783845b998769f77e87a52
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2023-12-15 23:04:29 +0100

    Update 37showsavedq&atestportalintest-result.js

--- 37/37showsavedq&atestportalintest-result.js
+++ 37/37showsavedq&atestportalintest-result.js
@@ -1,33 +1,3 @@
-const elements = document.querySelectorAll('.timer-tile-properties-container .mdc-property-value');
-const testname = document.querySelector('.test-name').innerText;
-
-if (elements.length > 0) {
-  const lastElementContent = elements[elements.length - 1].textContent.trim();
-
-
-  if (localStorage.getItem(`${lastElementContent} ${testname}`)) {
-    console.log('Klucz istnieje w local storage.');
-
-
-    const dataFromLocalStorage = localStorage.getItem(`${lastElementContent} ${testname}`);
-
-
-    if (dataFromLocalStorage) {
-      let data = JSON.parse(dataFromLocalStorage);
-
-
-      if (Array.isArray(data)) {
-        const questionsListTopBar = document.querySelector('.hidden-info');
-        questionsListTopBar.innerHTML = ''; // Wyczyść zawartość elementu
-        questionsListTopBar.style.display = 'block';
-        questionsListTopBar.style.fontFamily = '"Aktiv Grotesk", sans-serif';
-        let numerpytania = 0
-        
-//        var css = '.hidden-info img { max-width: 100%; height: auto; }';
-        var css = '.hidden-info img { max-width: -webkit-fill-available; height: auto; }';
-        var style = document.createElement('style');
-        style.appendChild(document.createTextNode(css));
-        document.head.appendChild(style);
 
             const copytoclipboard = document.createElement('button');
             copytoclipboard.innerText = 'Skopiuj do schowka';
@@ -37,71 +7,6 @@ if (elements.length > 0) {
             copytoclipboard.style.position = 'absolute';
             logincard.appendChild(copytoclipboard);
             copytoclipboard.addEventListener('click', () => {
-            alert("Skopiowano")
               navigator.clipboard.writeText(`Object.entries({"${selectedValue}":${JSON.stringify(localStorage.getItem(`${selectedValue}`))}}).forEach(([k,v])=>localStorage.setItem(k,v))`);
+              alert("Skopiowano")
             });
-
-
-        data.forEach(obj => {
-          const container = document.createElement('div');
-
-
-          Object.entries(obj).forEach(([key, value]) => {
-            if (key === "Nazwa testu") {
-              const h1 = document.createElement('h1');
-              h1.textContent = `${key}: ${value}`;
-              container.appendChild(h1);
-            } else if (key === "questionHTML") {
-              numerpytania = numerpytania + 1; // Update numerpytania without 'let'
-              const p = document.createElement('p');
-              p.innerHTML = `<h2>Pytanie ${numerpytania}:</h2> ${value}`;
-              container.appendChild(p);
-            } else  if (key === "answers") {
-                
-              const p = document.createElement('p');
-              p.innerHTML = `<strong>Odpowiedzi:</strong>`;
-
-
-              if (Array.isArray(value)) {
-                const ul = document.createElement('ul');
-                value.forEach(answer => {
-                  const li = document.createElement('li');
-                  li.textContent = answer;
-                  ul.appendChild(li);
-                });
-                p.appendChild(ul);
-              } else {
-                const div = document.createElement('div');
-                div.innerHTML = value;
-                p.appendChild(div);
-              }
-
-
-              container.appendChild(p);
-            } else {
-                const p = document.createElement('p');
-                p.innerHTML = `<strong>${value}</strong> ${value}`;
-            }
-          });
-
-
-          questionsListTopBar.appendChild(container);
-
-
-          // Dodaj odstęp między poszczególnymi elementami
-          const spacer = document.createElement('hr');
-          questionsListTopBar.appendChild(spacer);
-        });
-      } else {
-        console.error('Dane w local storage nie są w formie tablicy.');
-      }
-    } else {
-      console.error('Nie znaleziono danych w local storage pod podanym kluczem.');
-    }
-  } else {
-    console.log('Klucz nie istnieje w local storage.');
-  }
-} else {
-  console.log('Nie znaleziono żadnych elementów z tą klasą.');
-}
-
