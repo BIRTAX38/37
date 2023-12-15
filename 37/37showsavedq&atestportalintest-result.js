5e66162957859158e417da54ef39786148ebece3
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2023-12-15 23:16:56 +0100

    Update 37showsavedq&atestportalintest-result.js

--- 37/37showsavedq&atestportalintest-result.js
+++ 37/37showsavedq&atestportalintest-result.js
@@ -33,12 +33,10 @@ if (elements.length > 0) {
         const copytoclipboard = document.createElement('button');
         copytoclipboard.innerText = 'Skopiuj do schowka';
         copytoclipboard.classList.add('button37');
-        copytoclipboard.style.minWidth = '435px';
-        copytoclipboard.style.height = '3.5%';
-        copytoclipboard.style.position = 'absolute';
-        logincard.appendChild(copytoclipboard);
+        copytoclipboard.style.minWidth = '100%';
+        questionsListTopBar.appendChild(copytoclipboard);
         copytoclipboard.addEventListener('click', () => {
-          navigator.clipboard.writeText(`Object.entries({"${selectedValue}":${JSON.stringify(localStorage.getItem(`${selectedValue}`))}}).forEach(([k,v])=>localStorage.setItem(k,v))`);
+          navigator.clipboard.writeText(`Object.entries({"${lastElementContent} ${testname}":${JSON.stringify(localStorage.getItem(`${lastElementContent} ${testname}`))}}).forEach(([k,v])=>localStorage.setItem(k,v))`);
           alert("Skopiowano")
         });
 
