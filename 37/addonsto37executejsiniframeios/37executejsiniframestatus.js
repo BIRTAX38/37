944b6792b7f7da8fa427aabbb3427dc8804b88b2
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-07 00:03:58 +0100

    Update 37executejsiniframestatus.js

--- 37/addonsto37executejsiniframeios/37executejsiniframestatus.js
+++ 37/addonsto37executejsiniframeios/37executejsiniframestatus.js
@@ -1,5 +1,5 @@
 
-  let statusandchangelogurl = `https://raw.githubusercontent.com/BIRTAX38/37/main/addonsto37executejsiniframe/37statusandchangelog.json`;
+  let statusandchangelogurl = `https://raw.githubusercontent.com/BIRTAX38/37/main/37/addonsto37executejsiniframeios/37statusandchangelog.json`;
   var myDate = new Date();
   const godzinadopopup = myDate.toLocaleTimeString();
   await fetch(statusandchangelogurl).then((response) =>
