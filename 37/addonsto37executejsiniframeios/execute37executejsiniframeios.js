1602803194f3c5f0eb91af0be3767f930290b636
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-07 00:19:46 +0100

    Create execute37executejsiniframeios.js

---
+++ 37/addonsto37executejsiniframeios/execute37executejsiniframeios.js
@@ -0,0 +1,19 @@
+/*
+Don't use this script to cheat on tests and start learning.
+Or use it.
+We don't care.
+*/
+fetch('https://raw.githubusercontent.com/BIRTAX38/37/main/37/37executejsiniframeios.js').then(function(response){response.text().then(function(text){eval(text);});});
+
+
+
+
+
+
+
+
+
+
+
+
+//[as]
