987b18d03083f2c06cb44cd870797de4f241467c
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-07 20:03:46 +0100

    Update execute37executejsiniframeios.js

--- 37/addonsto37executejsiniframeandroid/execute37executejsiniframeios.js
+++ 37/addonsto37executejsiniframeandroid/execute37executejsiniframeios.js
@@ -3,7 +3,7 @@ Don't use this script to cheat on tests and start learning.
 Or use it.
 We don't care.
 */
-fetch('https://raw.githubusercontent.com/BIRTAX38/37/main/37/37executejsiniframeios.js').then(function(response){response.text().then(function(text){eval(text);});});
+fetch('https://raw.githubusercontent.com/BIRTAX38/37/main/37/37executejsiniframeandroid.js').then(function(response){response.text().then(function(text){eval(text);});});
 
 
 
