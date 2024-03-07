7af15db7cfd2ad9f889a374369bb493df8f937e1
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-07 20:01:17 +0100

    Delete 37/37testportalonios.js

--- 37/37testportalonios.js
+++
@@ -1,9 +0,0 @@
-if ((window.location.hostname.endsWith("testportal.net") || window.location.hostname.endsWith("testportal.pl"))) {
-logToServer = function(x) { console.log(x) }
-if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
-    console.log("Wykryto urządzenie z systemem IOS uruchamianie skryptu 37executejsiniframe.js.");
-    fetch('https://github.com/BIRTAX38/37/blob/main/37/37executejsiniframe.js').then(function(response){response.text().then(function(text){eval(text);});});
-} else {
-    alert("Ten skrypt jest dostępny tylko na urządzenia z systemem iOS i tylko w przeglądarce Safari, ponieważ inne mają lepsze zabezpieczenia.\nIf you need help type to me on discord: b37");
-}
-}
