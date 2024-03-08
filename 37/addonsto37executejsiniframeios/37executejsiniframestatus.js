9fae5669350708acfefa124c2a3f0e35ab4c1986
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-08 18:09:56 +0100

    Update 37executejsiniframestatus.js

--- 37/addonsto37executejsiniframeios/37executejsiniframestatus.js
+++ 37/addonsto37executejsiniframeios/37executejsiniframestatus.js
@@ -5,7 +5,7 @@ function fetchData() {
         return response.json();
       })
       .then(function(json) {
-        const textfromjson = `<hr> <b>Status: ${json.status}</b> <br /> <br /> Changelog: ${json.changelog} <br /> <br /> ${json.text} <br /> <br /> <b>By: ${json.author}</b>`;
+        const textfromjson = `<hr> <b>Status: ${json.status}</b> <br /> <br /> Changelog: <br />${json.changelog} <br /> <br /> ${json.text} <br /> <br /> <b>By: ${json.author}</b>`;
         document.getElementsByClassName('test-card-body')[0].innerHTML += textfromjson;
       })
       .catch(function(error) {
