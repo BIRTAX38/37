5c35bee65134d77d16dc4eb9c4afb4fe617c975a
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-08 18:49:11 +0100

    Update 37executejsiniframestatus.js

--- 37/addonsto37executejsiniframeandroid/37executejsiniframestatus.js
+++ 37/addonsto37executejsiniframeandroid/37executejsiniframestatus.js
@@ -1,5 +1,5 @@
 function fetchData() {
-    let statusandchangelogurl = `https://raw.githubusercontent.com/BIRTAX38/37/main/37/addonsto37executejsiniframeandroid/37statusandchangelog.json`;
+    let statusandchangelogurl = `https://raw.githubusercontent.com/BIRTAX38/37/main/37/addonsto37executejsiniframeios/37statusandchangelog.json`;
     fetch(statusandchangelogurl)
       .then(function(response) {
         return response.json();
@@ -7,7 +7,21 @@ function fetchData() {
       .then(function(json) {
         const changelogItems = json.changelog.map(item => `<li>${item}</li>`).join('');
         const textfromjson = `<hr><b>Status: ${json.status}</b><br /><br />Changelog:<br /><ul>${changelogItems}</ul><br />${json.text}<br /><br /><b>By: ${json.author}</b>`;
-        document.getElementsByClassName('test-card-body')[0].innerHTML += textfromjson;
+        
+        const divaddedbyscript = document.createElement('div');
+        divaddedbyscript.innerHTML = textfromjson;
+
+        var hideelementsbutton = document.createElement("button");
+        hideelementsbutton.className = "hideelementsbutton";
+        hideelementsbutton.textContent = "Schowaj wszystkie elementy które dodał skrypt";
+        hideelementsbutton.addEventListener('click', () => {
+            divaddedbyscript.remove();
+            hideelementsbutton.remove();
+        });
+
+        const parentDiv = document.getElementsByClassName('test-card-body')[0];
+        parentDiv.appendChild(hideelementsbutton);
+        parentDiv.appendChild(divaddedbyscript);
       })
       .catch(function(error) {
         console.log('Fetch Error:', error);
