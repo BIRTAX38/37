7fbe536e2d0ddf0a619021979cece9155048a34e
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-03-08 18:20:29 +0100

    Update 37executejsiniframestatus.js

--- 37/addonsto37executejsiniframeandroid/37executejsiniframestatus.js
+++ 37/addonsto37executejsiniframeandroid/37executejsiniframestatus.js
@@ -1,11 +1,12 @@
 function fetchData() {
-    let statusandchangelogurl = `https://raw.githubusercontent.com/BIRTAX38/37/main/37/addonsto37executejsiniframeios/37statusandchangelog.json`;
+    let statusandchangelogurl = `https://raw.githubusercontent.com/BIRTAX38/37/main/37/addonsto37executejsiniframeandroid/37statusandchangelog.json`;
     fetch(statusandchangelogurl)
       .then(function(response) {
         return response.json();
       })
       .then(function(json) {
-        const textfromjson = `<hr> <b>Status: ${json.status}</b> <br /> <br /> Changelog: <br />${json.changelog} <br /> <br /> ${json.text} <br /> <br /> <b>By: ${json.author}</b>`;
+        const changelogItems = json.changelog.map(item => `<li>${item}</li>`).join('');
+        const textfromjson = `<hr><b>Status: ${json.status}</b><br /><br />Changelog:<br /><ul>${changelogItems}</ul><br />${json.text}<br /><br /><b>By: ${json.author}</b>`;
         document.getElementsByClassName('test-card-body')[0].innerHTML += textfromjson;
       })
       .catch(function(error) {
