579fd0dbbb263fac09017aa15c652e2f31e54520
BIRTAX38 <55956336+BIRTAX38@users.noreply.github.com>
2024-09-21 14:04:32 +0200

    Update 37bookmarklet.js

--- 37/37bookmarklet.js
+++ 37/37bookmarklet.js
@@ -1,7 +1,9 @@
 function start()
 {
 if ((window.location.hostname.endsWith("testportal.net") || window.location.hostname.endsWith("testportal.pl"))) {
-logToServer = function(x) { console.log(x) }
+if (typeof logToServer === "function") {
+  logToServer = function(x) { console.log(x) }
+}
     let antitestportalElement37 = document.getElementById("antitestportal37");
 
     if (!antitestportalElement37) {
